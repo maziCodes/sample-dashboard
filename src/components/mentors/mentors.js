c112183f18cf61e0a7632502d6473bc45705a87a
mazicodes <bristonmekus@gmail.com>
2024-01-11 13:54:06 +0100

    modify image style

--- src/components/mentors/mentors.js
+++ src/components/mentors/mentors.js
@@ -5,7 +5,7 @@ import "./mentors.css"
 function Mentors() {
     const mentors = [
         {
-            image: 'https://media.sciencephoto.com/c0/34/78/25/c0347825-800px-wm.jpg',
+            image: 'https://images.unsplash.com/photo-1573496130141-209d200cebd8?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             name: '',
             location: '',
             interest: '',
